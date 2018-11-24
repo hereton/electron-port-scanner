5bfd30da5b5ded813937f985cc121cc3435b1410
Totsapon Menkul <totsapon.m@ku.th>
2018-11-24 21:09:43 +0700

    add guitar code

--- public/script/index.js
+++ public/script/index.js
@@ -44,7 +44,7 @@ $(() => {
         }
       }, 2000)
     } catch (error) {
-      alert('Error')
+      alert(error)
       unLoadTrigger()
     }
   }
@@ -69,27 +69,28 @@ getHostArray = host => {
   let hosts = []
   let result = host.trim().split(',')
   console.log(result)
-  result.map(hostName => {
+  result.map(async hostName => {
     console.log(hostName)
     if (hostName.includes('/')) {
-      let netMask = '00000000000000000000000000000000'
-      let s = hostName.split('/')
-      console.log(s[1])
-      let lastIndex = 0
-      netMask = netMask.substring(0, parseInt(s[1])).replace(/0/g, '1') + netMask.substring(parseInt(s[1]), netMask.length)
-      let network = s[0]
-        .split('.')
-        .map((e, index) => {
-          let a = ('00000000' + Number(e).toString(2)).slice(-8)
-          // console.log('a', a)
-          // console.log('netmask', netMask.substring(lastIndex, (index + 1) * 8))
-          a = parseInt(a, 2) & parseInt(netMask.substring(lastIndex, (index + 1) * 8), 2)
-          lastIndex = (index + 1) * 8
+      let subNetAndMask = hostName.split('/')
+      let mask = subNetAndMask[1]
+      let subNet = subNetAndMask[0].split('.')
+      console.log('mask', mask)
+      console.log('subnet', subNet)
+      hosts = await subnetCal(parseInt(subNet[0]), parseInt(subNet[1]), parseInt(subNet[2]), parseInt(subNet[3]), parseInt(mask), hosts)
+      // netMask = netMask.substring(0, parseInt(s[1])).replace(/0/g, '1') + netMask.substring(parseInt(s[1]), netMask.length)
+      // let network = s[0]
+      //   .split('.')
+      //   .map((e, index) => {
+      //     let a = ('00000000' + Number(e).toString(2)).slice(-8)
+      //     // console.log('a', a)
+      //     // console.log('netmask', netMask.substring(lastIndex, (index + 1) * 8))
+      //     a = parseInt(a, 2) & parseInt(netMask.substring(lastIndex, (index + 1) * 8), 2)
+      //     lastIndex = (index + 1) * 8
 
-          return a
-        })
-        .join('.')
-      hosts.push(network)
+      //     return a
+      //   })
+      //   .join('.')
     } else {
       hosts.push(hostName)
     }
@@ -155,4 +156,81 @@ async function addElement(data) {
       })
     }
   })
+}
+
+function maskShift(mask, back = 0) {
+  return ((Math.pow(2, mask) - 1) << (32 - mask)) >>> back
+}
+
+function printShift(num) {
+  out = ''
+  while (num != 0) {
+    out = (num % 2) + out
+    num = num >>> 1
+  }
+
+  if (out == '') {
+    out = '0'
+  }
+
+  return out
+}
+
+// Input p1.p2.p3.p4/mask
+function isValid(p1, p2, p3, p4, mask) {
+  if (!Number.isInteger(p1) || !Number.isInteger(p2) || !Number.isInteger(p3) || !Number.isInteger(p4) || !Number.isInteger(mask)) {
+    return false
+  }
+  if (p1 < 1 || p2 < 0 || p3 < 0 || p4 < 0 || mask < 8) {
+    return false
+  }
+  return true
+}
+
+function subnetCal(p1, p2, p3, p4, mask, hosts) {
+  console.log('subnet called', p1, p2, p3, p4, mask)
+  if (!isValid(p1, p2, p3, p4, mask)) {
+    return 'invalid ip format'
+  }
+  classMask = 0
+  // Class A
+  if (p1 > 0 && p1 <= 127) {
+    classMask = 8
+    // Class B
+  } else if (p1 > 127 && p1 <= 191) {
+    classMask = 16
+    // Class C
+  } else if (p1 > 191 && p1 <= 223) {
+    classMask = 24
+  } else {
+    return 'Class D and E are not allowed'
+  }
+
+  if (mask < classMask) {
+    return 'invalid ip format'
+  }
+
+  // Important: Don't delete `>>> 0` because it will cast signed int to unsigned int.
+  ip = ((((p1 << 24) >>> 0) + ((p2 << 16) >>> 0) + ((p3 << 8) >>> 0) + p4) >>> 0) >>> 0
+
+  netid = (ip & maskShift(classMask)) >>> (32 - classMask)
+  subnetid = (ip & maskShift(mask - classMask, classMask)) >>> (32 - mask)
+  hostid = (ip & maskShift(32 - mask, mask)) >>> 0
+
+  console.log(printShift(netid) + ' ' + printShift(subnetid) + ' ' + printShift(hostid))
+  if (hostid != 0) {
+    return 'invalid subnet'
+  }
+
+  for (i = 0; i < Math.pow(2, 32 - mask) - 2; i++) {
+    hostid = (hostid + 1) >>> 0
+    res = ((netid << (32 - classMask)) >>> 0) + ((subnetid << (32 - mask)) >>> 0) + (hostid >>> 0)
+    i1 = res >>> 24
+    i2 = res & (maskShift(8, 8) >>> 16)
+    i3 = res & (maskShift(8, 16) >>> 8)
+    i4 = res & (maskShift(8, 24) >>> 0)
+    hosts.push(i1 + '.' + i2 + '.' + i3 + '.' + i4)
+  }
+
+  return hosts
 }
